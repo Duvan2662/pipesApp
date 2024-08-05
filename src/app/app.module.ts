30ab3b9650468a0499750e843f9bc9282e14e94b
Duvan <duvanriano2626@gmail.com>
2024-08-05 14:35:42 -0500

    Cambiar el idioma de manera global y manualmente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -8,6 +8,16 @@ import { SharedModule } from "./shared/shared.module";
 
 
 import { AppComponent } from './app.component';
+
+
+//configuracion del locale de la app
+import localeEsCol from "@angular/common/locales/es-CO";
+import localeEsFrCa from "@angular/common/locales/fr-CA";
+
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(localeEsCol);
+registerLocaleData(localeEsFrCa);
 
 
 
@@ -24,7 +34,9 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
 ],
   providers: [
-    provideClientHydration()
+    {
+      provide: LOCALE_ID, useValue :'es-CO'
+    }
   ],
   bootstrap: [AppComponent]
 })
