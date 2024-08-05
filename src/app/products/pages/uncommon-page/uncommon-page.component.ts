8fff14e9b5e737be35c72e2ea4657c1637088ad5
Duvan <duvanriano2626@gmail.com>
2024-08-05 15:28:35 -0500

    Pipe para cuando se quiere alternar el texto si es masculino o femenino o en ocasiones especificas i18nSelect

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  //i18n Select
+
+  public name : string = 'Duvan';
+  public gender : 'male'| 'female' = 'male';
+  public invitationMap = {
+    male : 'invitarlo',
+    female : 'invitarla'
+  }
+
+
+
+  /**
+   * ChangeClient
+   * metodo para cambiar la persona
+   */
+  public ChangeClient = () => {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
 }
