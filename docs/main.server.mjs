c9ca8bc9eddc7c8436128a328730ffcc408e98b2
Duvan <duvanriano2626@gmail.com>
2024-08-07 11:43:23 -0500

    Se configuro el tamaño de type initial en el angular.json, luego se crearon los comandos para la generacion de la carpeta Docs para subir la carpeta a githubPages

---
+++ docs/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-B7SRCSUD.mjs";import"./chunk-5Y24LQXA.mjs";import"./chunk-KVOJIMNR.mjs";import"./chunk-KJIVDT5Z.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
